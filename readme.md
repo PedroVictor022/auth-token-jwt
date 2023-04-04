0792fe5cc283025f184cdea030cc9b7b15331fc5
PedroVictor022 <pedro20002victor@gmail.com>
2023-04-04 16:03:16 -0300

    migrate db

---
+++ readme.md
@@ -0,0 +1,6 @@
+# FLUXO DE AUTH COM TS E JWT
+
+--
+Primeiro rode o `npx tsc --init` para iniciar o tsconfig.json
+Para criar o banco, `npx prisma init` e apos criar o model rode um `npx prisma migrate dev`
+preencha as informacoes
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { json } from "express";
+import { route } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(route);
 
 app.listen(3333, () => {
-  console.log("Server is running on port " + `http://localhost:3333`);
+  console.log("Server is running on port http://localhost:3333");
 })
