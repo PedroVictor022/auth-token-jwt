abcdadd2a873468780d6947477fc363443f46443
PedroVictor022 <pedro20002victor@gmail.com>
2023-04-04 16:23:39 -0300

    FEATURE - encrypted password

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import prisma from "../utils/prismaClient";
+import { hash } from "bcryptjs";
 
 class UserController {
   async store(req: Request, res: Response) {
     const { name, email, password } = req.body;
+
+    // create a encrypted password
+    const hash_password = await hash(password, 8);
+
     const user = await prisma.user.create({
       data: {
-        name, email, password
+        name,
+        email,
+        password: hash_password
       }
     })
     return res.json(user);
