5b81f8f6f1448b5a4265ef0417cd5b0d29dacd79
PedroVictor022 <pedro20002victor@gmail.com>
2023-04-08 14:01:42 -0300

    install cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express, { json } from "express";
 import { route } from "./routes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(route);
 
